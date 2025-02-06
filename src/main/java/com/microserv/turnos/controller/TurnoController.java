a8b205c269bdcb672c7652e31572c66743b19ca9
Martín Alejandro Romero <martin.rome99@gmail.com>
2025-02-05 22:49:19 -0300

    parte uno de microservicios

---
+++ src/main/java/com/microserv/turnos/controller/TurnoController.java
@@ -0,0 +1,49 @@
+package com.microserv.turnos.controller;
+
+import com.microserv.turnos.model.Turno;
+import com.microserv.turnos.service.ITurnoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@RestController
+@RequestMapping("/turnos")
+public class TurnoController {
+    @Autowired
+    private ITurnoService turnoServ;
+
+    //crear nuevo turno
+    @PostMapping ("/crear")
+    public String crearTurno (@RequestBody LocalDate fecha,
+                              @RequestBody String tratamiento,
+                              @RequestBody String dniPaciente){
+        turnoServ.saveTurno(fecha, tratamiento, dniPaciente);
+        return "Turno creado correctamente";
+    }
+
+    @GetMapping ("/traer")
+    public List<Turno> traerTurnos(){
+        return turnoServ.getTurnos();
+    }
+
+    @DeleteMapping ("/borrar/{id}")
+    public String deleteTurno (@PathVariable Long id){
+        turnoServ.deleteTurno(id);
+        return "Turno eliminado correctamente";
+    }
+
+    @PutMapping ("/editar/{idOriginal}")
+    public Turno editTurno (@PathVariable Long idOriginal,
+                            @RequestBody Turno turnoEditar){
+        turnoServ.editTurno(idOriginal, turnoEditar);
+        Turno turnoEditado = turnoServ.findTurno(idOriginal);
+        return turnoEditado;
+    }
+
+    @GetMapping("/traer/{id}")
+    public Turno traerTurno(@PathVariable Long id){
+        return turnoServ.findTurno(id);
+    }
+}
